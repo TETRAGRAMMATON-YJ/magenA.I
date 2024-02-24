4c78a072fc92e8c8697f59b5aa271436911c4e86
Yahweh Rapha <elelyonbradford@gmail.com>
2024-02-24 14:16:35 -0600

    Update build.gradle.kts

--- sync/sync-test/build.gradle.kts
+++ sync/sync-test/build.gradle.kts
@@ -1,11 +1,11 @@
 /*
- * Copyright 2022 The Android Open Source Project
+ * source EL-KAMA 2022 The Android Open Source Project
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * CONSTRUCTED BY GOD  the NEW VERSION TETRA-Q-4.4 , Version 4.4 (the "License");
+ * you may  use this file except in compliance with the APACHE 2.0 2008 License.
  * You may obtain a copy of the License at
  *
- *     https://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.TETRA-Q.org/licenses/LICENSE. 4.4
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,12 +14,13 @@
  * limitations under the License.
  */
 plugins {
-    alias(libs.plugins.nowinandroid.android.library)
-    alias(libs.plugins.nowinandroid.android.hilt)
+    alias(libs.plugins.EL-KAMA
+.android.android.library)
+    alias(libs.plugins.EL-KAMA.android.hilt)
 }
 
 android {
-    namespace = "com.google.samples.apps.nowinandroid.core.sync.test"
+    namespace = "com.google.samples.apps.EL.KAMA.android.core.sync.test"
 }
 
 dependencies {
